1a163ab2e1fe670cafefda546900c8e71e977756
Franklin Cabrera <jamess166@hotmail.com>
2021-11-06 00:55:23 -0500

    Abrir archivos ifc

--- bundle.js
+++ bundle.js
@@ -103447,14 +103447,27 @@ class IfcViewerAPI {
     }
 }
 
+//Creando la escena y los ejes
 const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({container, backgroundColor: new Color(255,255,255)});
+const viewer = new IfcViewerAPI({container, backgroundColor: new Color(243,243,247)});
 viewer.addAxes();
 viewer.addGrid();
 
+//Obtener el input
 const GUI = {
     input: document.getElementById('file-input'),
     loader: document.getElementById('loader-button')
 };
 
+//Abrir modelos con el boton
 GUI.loader.onclick = () => GUI.input.click();
+GUI.input.onchange = () => {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    viewer.IFC.loadIfcUrl(url);
+};
+
+//Precargar modelos
+viewer.IFC.loadIfcUrl('test.ifc');
+
+//Seleccionar elementos
